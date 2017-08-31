87b870dc85ee30da1e1bf234d9bd106c24a4c053
Chris Whetzel <cewhetzel@gmail.com>
2017-08-31 09:43:02 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GoogleMapsBeginner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.browser_Google = new System.Windows.Forms.WebBrowser();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(897, 434);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(104, 42);
+            this.btn_Exit.TabIndex = 0;
+            this.btn_Exit.TabStop = false;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // browser_Google
+            // 
+            this.browser_Google.Location = new System.Drawing.Point(12, 42);
+            this.browser_Google.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser_Google.Name = "browser_Google";
+            this.browser_Google.ScrollBarsEnabled = false;
+            this.browser_Google.Size = new System.Drawing.Size(989, 386);
+            this.browser_Google.TabIndex = 1;
+            this.browser_Google.TabStop = false;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(463, 12);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(80, 26);
+            this.btn_Search.TabIndex = 2;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(445, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 488);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.browser_Google);
+            this.Controls.Add(this.btn_Exit);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Google Maps Lookup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.WebBrowser browser_Google;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleMapsBeginner
+{
+    public partial class Form1 : Form
+    {
+        /* TODO: Chris
+         * Issue - c# you seem to be using an unsupported browser
+         * Fix - To fix this, it seems there needs to be a registry change. We need to do this
+         * progmatically, mostly so I can get practice with c# programming.
+         */
+        String page = "https://www.google.com/maps/place/8407+River+Park+Rd,+Bowie,+MD+20715/@39.004516,-76.7518843,17z/data=!3m1!4b1!4m5!3m4!1s0x89b7e93b757e2835:0x8bff0b5a04cc610e!8m2!3d39.0045119!4d-76.7496903";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            ExitApplication exitApplication = new ExitApplication();
+            exitApplication.SaveData();
+            exitApplication.Exit();
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            setup_Page();
+        }
+
+        private void setup_Page()
+        {
+            browser_Google.Navigate(page);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setup_Page();
+        }
+
+        // Set a registry DWORD value.
+        private void SetRegistryDword(string key_name, string value_name, int value)
+        {
+            // Open the key.
+            RegistryKey key =
+                Registry.CurrentUser.OpenSubKey(key_name, true);
+            if (key == null)
+                key = Registry.CurrentUser.CreateSubKey(key_name,
+                    RegistryKeyPermissionCheck.ReadWriteSubTree);
+
+            // Set the desired value.
+            key.SetValue(value_name, value, RegistryValueKind.DWord);
+
+            key.Close();
+        }
+    }
+    class MyBrowser
+    {
+        String page = "";
+
+        public string Page { get => page; set => page = value; }
+    }
+    class XMLData
+    {
+        public void Add()
+        {
+
+        }
+        public void Remove()
+        {
+
+        }
+        public string Retrieve()
+        {
+            return "test";
+        }
+    }
+    public class ExitApplication
+    {
+        public void SaveData()
+        {
+
+        }
+        public void Exit()
+        {
+            Application.Exit();
+        }
+    }
+}
